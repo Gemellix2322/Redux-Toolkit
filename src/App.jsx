de07b07b84533a301f2553936841703bbac53c83
Gemellix2322 <guilhermegemelli232@gmail.com>
2025-02-27 10:46:35 -0300

    feat: added input and remove function

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import './App.css'
+import { MovieInput } from './components/MovieInput';
+import { MovieList } from './components/MovieList';
 
 function App() {
-  return <></>; 
+  return (
+    <>
+      <MovieInput />
+      <MovieList />
+    </>
+  );
 }
 
 export default App
