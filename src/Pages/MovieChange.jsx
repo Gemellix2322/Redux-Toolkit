69f39e732d401cc5cb646ce59aecf7f4cdc7337f
Gemellix2322 <guilhermegemelli232@gmail.com>
2025-02-28 11:47:50 -0300

    feat: css better

--- src/Pages/MovieChange.jsx
+++ src/Pages/MovieChange.jsx
@@ -2,7 +2,8 @@ import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { changeMovie } from "../movieSlice";
 import { useNavigate, useParams } from "react-router-dom";
-import { Button, TextField, Paper } from "@mui/material";
+import { Button, TextField, Paper, Box } from "@mui/material";
+import BackIcon from '@mui/icons-material/ArrowBack';
 
 const MovieChange = () => {
     const navigate = useNavigate();
@@ -21,8 +22,21 @@ const MovieChange = () => {
         }
     }
 
+    const handleback = () => {
+        navigate('/')
+    }
+
     return (
         <>
+            <Box sx={{ position: 'fixed', top: 50, left: 50 }}>
+                <Button
+                    onClick={handleback}
+                    startIcon={<BackIcon />}
+                    sx={{ position: 'absolute', left: 0, top: 0 }}
+                >
+                    Back
+                </Button>
+            </Box>
             <Paper sx={{ p: 2, borderRadius: 2, boxShadow: 3 }}>
                 <TextField
                     onChange={(e) => setMovieName(e.target.value)}
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -2,6 +2,8 @@ import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { removeMovie } from "../movieSlice";
 import { useNavigate } from "react-router-dom";
+import DeleteIcon from '@mui/icons-material/Delete';
+import CreateIcon from '@mui/icons-material/Create';
 import { Box, Button, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
 
 export const MovieList = () => {
@@ -39,9 +41,9 @@ export const MovieList = () => {
                             <TableRow key={movies.id} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
                                 <TableCell align="center">{movies.id}</TableCell>
                                 <TableCell>{movies.name}</TableCell>
-                                <TableCell align="left">
-                                    <Button onClick={handleRemoveMovie}>Delete Movie</Button>
-                                    <Button onClick={handleChangeMovie}>Change Movie</Button>
+                                <TableCell align="left" sx={{display: "flex", gap: 2}}>
+                                    <Button startIcon={<DeleteIcon />} variant="outlined" onClick={handleRemoveMovie}>Delete Movie</Button>
+                                    <Button startIcon={<CreateIcon />} variant="outlined" onClick={handleChangeMovie}>Change Movie</Button>
                                 </TableCell>
                             </TableRow>
                         ))}
